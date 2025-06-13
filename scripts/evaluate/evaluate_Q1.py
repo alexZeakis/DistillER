2f84a7d60a7b96f83499fc74b3bfdfcdb73fb5d0
alexZeakis <alzeakis@gmail.com>
2025-06-13 13:44:29 +0300

    Updated Q1

--- scripts/evaluate/evaluate_Q1.py
+++ scripts/evaluate/evaluate_Q1.py
@@ -4,18 +4,20 @@ from eval_utils import prepare_pt_file, prepare_plm_train_file, get_inference_ti
 
 ################### COMPARING LLMS ON TRAIN DATA #########################
 
-path = '../../log/matching/llm/'
-path2 = '../../log/matching/hybrid/'
+path = '../../log/matching/annotate/'
 total_df_train = pd.DataFrame()
-total_df_train['Llama3.1:8b'] = prepare_pt_file(path+'noisy_llama_8/partial_responses/')
-total_df_train['Llama3.1:70b'] = prepare_pt_file(path+'noisy_llama_70/partial_responses/')
-# total_df_train['Qwen2.5:7b'] = prepare_pt_file(path+'noisy_qwen_7/partial_responses/')
-total_df_train['Qwen2.5:14b'] = prepare_pt_file(path+'noisy_qwen_14/partial_responses/')
-total_df_train['Qwen2.5:32b'] = prepare_pt_file(path+'noisy_qwen_32/partial_responses/')
+total_df_train['Llama3.1:8b'] = prepare_pt_file(path+'llama_8/partial_responses/')
+total_df_train['Llama3.1:70b'] = prepare_pt_file(path+'llama_70/partial_responses/')
+# total_df_train['Qwen2.5:7b'] = prepare_pt_file(path+'qwen_7/partial_responses/')
+total_df_train['Qwen2.5:14b'] = prepare_pt_file(path+'qwen_14/partial_responses/')
+total_df_train['Qwen2.5:32b'] = prepare_pt_file(path+'qwen_32/partial_responses/')
 
-# total_df_train['Qwen2.5:7b'] = prepare_ft_file(path2+'/noisy_qwen_7/partial_responses/')
-total_df_train['PLM-Ground'] = prepare_plm_train_file(path2+'ground/')
-total_df_train['PLM-70b'] = prepare_plm_train_file(path2+'noisy_llama_70/')
+# total_df_train['Qwen2.5:7b'] = prepare_ft_file(path+'/qwen_7/partial_responses/')
+total_df_train['Hybrid-SMiniLM-Ground'] = prepare_plm_train_file(path+'sminilm/ground/')
+total_df_train['Hybrid-SMiniLM-70b'] = prepare_plm_train_file(path+'sminilm/llama_70/')
+total_df_train['Hybrid-RoBERTa-Ground'] = prepare_plm_train_file(path+'roberta/ground/')
+total_df_train['Hybrid-RoBERTa-70b'] = prepare_plm_train_file(path+'roberta/llama_70/')
+
 
 total_df_train = prepare_df(total_df_train)
 
@@ -23,14 +25,18 @@ latex_code = total_df_train.to_latex(index=True, escape=False, multirow=False)
 
 
 total_df_time = {}
-total_df_time['Llama3.1:8b'] = get_inference_time(path+'noisy_llama_8/partial_responses/').sum()
-total_df_time['Llama3.1:70b'] = get_inference_time(path+'noisy_llama_70/partial_responses/').sum()
-total_df_time['Qwen2.5:14b'] = get_inference_time(path+'noisy_qwen_14/partial_responses/').sum()
-total_df_time['Qwen2.5:32b'] = get_inference_time(path+'noisy_qwen_32/partial_responses/').sum()
-total_df_time['Hybrid-Inference'] = total_df_time['Llama3.1:70b'] * 0.2
-total_df_time['Hybrid-Training'] = get_plm_training_time(path2+'noisy_llama_70/')
-total_df_time['Hybrid-Testing'] = get_plm_testing_time(path2+'noisy_llama_70/')
-total_df_time['Hybrid-Sum'] = total_df_time['Hybrid-Inference'] + total_df_time['Hybrid-Training'] + total_df_time['Hybrid-Testing']
+total_df_time['Llama3.1:8b'] = get_inference_time(path+'llama_8/partial_responses/').sum()
+total_df_time['Llama3.1:70b'] = get_inference_time(path+'llama_70/partial_responses/').sum()
+total_df_time['Qwen2.5:14b'] = get_inference_time(path+'qwen_14/partial_responses/').sum()
+total_df_time['Qwen2.5:32b'] = get_inference_time(path+'qwen_32/partial_responses/').sum()
+total_df_time['Hybrid-SMiniLM-Inference'] = total_df_time['Llama3.1:70b'] * 0.2
+total_df_time['Hybrid-SMiniLM-Training'] = get_plm_training_time(path+'sminilm/llama_70/')
+total_df_time['Hybrid-SMiniLM-Testing'] = get_plm_testing_time(path+'sminilm/llama_70/')
+total_df_time['Hybrid-SMiniLM-Sum'] = total_df_time['Hybrid-SMiniLM-Inference'] + total_df_time['Hybrid-SMiniLM-Training'] + total_df_time['Hybrid-SMiniLM-Testing']
+total_df_time['Hybrid-RoBERTa-Inference'] = total_df_time['Llama3.1:70b'] * 0.2
+total_df_time['Hybrid-RoBERTa-Training'] = get_plm_training_time(path+'roberta/llama_70/')
+total_df_time['Hybrid-RoBERTa-Testing'] = get_plm_testing_time(path+'roberta/llama_70/')
+total_df_time['Hybrid-RoBERTa-Sum'] = total_df_time['Hybrid-RoBERTa-Inference'] + total_df_time['Hybrid-RoBERTa-Training'] + total_df_time['Hybrid-RoBERTa-Testing']
 total_df_time = pd.Series(total_df_time).to_frame()
 
 latex_code = total_df_time.to_latex(index=True, escape=False, multirow=False)
